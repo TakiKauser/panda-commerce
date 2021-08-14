5690aeb67e4f08ce9c63b7c286d8bf4cb23344b1
TakiKauser <takikauserbadhon@gmail.com>
2021-08-14 06:07:06 +0600

    h2 tags text color changed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// h2 texts color changed
+const sectionTitles = document.getElementsByClassName("section-title");
+
+for (const title of sectionTitles){
+    // console.log(title);
+    title.style.color = "white";
+}
